261d382a6a812a1fd9b5493cd0a5f2f161d55ad3
kulapard <kulapard@gmail.com>
2014-10-08 20:04:02 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ except:
     readme = None
 
 setup(
-    name='TornadIO2',
-    version='0.0.3',
+    name='KP-TornadIO2',
+    version='0.0.4',
     author='Serge S. Koval',
     author_email='serge.koval@gmail.com',
     packages=['tornadio2'],
